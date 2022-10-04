33f0d4dd2145fe01ba8ca6e462caee250ee9b4a9
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-10-04 19:40:46 +0530

    refactor: separate code in functions

--- 318-maximum-product-of-word-lengths/MaxProductWordLengthsBitMask.java
+++ 318-maximum-product-of-word-lengths/MaxProductWordLengthsBitMask.java
@@ -1,28 +1,11 @@
-class Solution {
+class MaxProductWordLengthsBitMask {
     public int maxProduct(String[] words) {
         int n = words.length;
         int maxLen = 0;
-        int[] bitmasks = new int[n];
         
-        for(int i = 0; i<n; i++) {
-            int value = bitmasks[i];
-            
-            String word = words[i];
-            int wordLen = word.length();
-            for(int j = 0; j<wordLen; j++) {
-                char ch = word.charAt(j);
-                
-                // This created a mask such that 
-                // if ch = 'a' => 1
-                // if ch = 'b' => 10
-                // if ch = 'c' => 100
-                // ...
-                // if ch == 'z' => 1000...00 (25 0s)
-                value |= (1 << ch - 'a');
-            }
-            bitmasks[i] = value;
-        }
+        int[] bitmasks = getWordMasks(words, n);
         
+        // Compares every word i with every other word j. [0 <= i < j < n]
         for(int i = 0; i<n-1; i++) {
             String firstWord = words[i];
             for(int j = 0; j<n; j++) {
@@ -40,7 +23,47 @@ class Solution {
         return maxLen;
     }
     
+    private int[] getWordMasks(String[] words, int n) {
+        int[] bitmasks = new int[n];
+        
+        for(int i = 0; i<n; i++) {
+            String word = words[i];
+            bitmasks[i] = generateWordMask(word);
+        }
+        
+        return bitmasks;
+    }
+    
     private boolean compareCharacters(String firstWord, String secondWord, int firstBitMask, int secondBitMask) {
         return (firstBitMask & secondBitMask) == 0;
+    }
+    
+    private int generateWordMask(String word) {
+        int value = 0;
+        int wordLen = word.length();
+
+        for(int j = 0; j<wordLen; j++) {
+            char ch = word.charAt(j);
+            int position = getCharacterPositionInBit(ch);
+            int mask = getCharacterMaskWithSetBit(position);
+                
+            // This creates a mask such that 
+            // if ch = 'a' => 1
+            // if ch = 'b' => 10
+            // if ch = 'c' => 100
+            // ...
+            // if ch == 'z' => 1000...00 (25 0s)
+            value |= mask;
+        }
+        
+        return value;
+    }
+    
+    private int getCharacterPositionInBit(char ch) {
+        return ch - 'a';
+    }
+    
+    private int getCharacterMaskWithSetBit(int position) {
+        return 1 << position;
     }
 }
