376d30bb0852583e96f482d928c630633f1f5596
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-11-18 22:57:17 +0530

    Create UglyNumber.java

---
+++ 263-ugly-number/UglyNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0) return false;
+        
+        while(n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+            if(n % 2 == 0) n /= 2;
+            else if(n % 3 == 0) n /= 3;
+            else n /= 5;
+        }
+        
+        return n == 1;
+    }
+}
