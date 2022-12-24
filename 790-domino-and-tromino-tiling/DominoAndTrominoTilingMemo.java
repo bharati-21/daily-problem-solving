4fef47047b8ac2d29b7dfc99a748050720567f35
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2022-12-24 22:38:12 +0530

    Update DominoAndTrominoTilingMemo.java

--- 790-domino-and-tromino-tiling/DominoAndTrominoTilingMemo.java
+++ 790-domino-and-tromino-tiling/DominoAndTrominoTilingMemo.java
@@ -1,6 +1,13 @@
 class Solution {
     int mod = (int) (1e9 + 7);
+    long[] memo, tromino;
     public int numTilings(int n) {
+        memo = new long[n+1];
+        tromino = new long[n+1];
+        
+        Arrays.fill(memo, -1);
+        Arrays.fill(tromino, -1);
+        
         return (int) numTilingsHelper(n) % mod;
     }
     
@@ -8,23 +15,27 @@ class Solution {
         if(n <= 0) return 0l;
         if(n <= 2) return n;
         
+        if(memo[n] != -1) return memo[n] % mod;
         
         // f(n-1) + f(n-2) + 2*g(n-2)
-        long vert = numTilingsHelper(n-1) % mod;
-        long hor = numTilingsHelper(n-2) % mod;
-        long tro = numTilingsTromino(n-2) % mod;
+        long verticalTile = numTilingsHelper(n-1) % mod;
+        long horizontalTile = numTilingsHelper(n-2) % mod;
+        long trominoTile = numTilingsTromino(n-2) % mod;
         
-        return (vert + hor + 2 * tro) % mod;
+        return memo[n] = (verticalTile + horizontalTile + 
+                          2 * trominoTile) % mod;
     }
     
     private long numTilingsTromino(int n) {
         if(n <= 0) return 0l;
         if(n == 1) return 1l;
-                
+        
+        if(tromino[n] != -1) return tromino[n];
+        
         // g(n) = g(n-1) + f(n-1)
-        long hor = numTilingsTromino(n-1) % mod;
-        long tro = numTilingsHelper(n-1) % mod;
+        long horizontalTile = numTilingsTromino(n-1) % mod;
+        long trominoTile = numTilingsHelper(n-1) % mod;
                 
-        return (hor + tro) % mod;
+        return tromino[n] = (horizontalTile + trominoTile) % mod;
     }
 }
